82aa838709317d9fbc4fa95443fed3bb4006b2af
mausek <112874771+KrzysztofGrabalski@users.noreply.github.com>
2024-02-05 11:54:08 +0100

    telefun

---
+++ telefun/telefun/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace telefun
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            colorDialog1 = new ColorDialog();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(33, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(215, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Podaj adres e-mail:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(33, 181);
+            label2.Name = "label2";
+            label2.Size = new Size(137, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Podaj hasło:";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(33, 271);
+            label3.Name = "label3";
+            label3.Size = new Size(244, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Potwierdź swoje hasło:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(25, 142);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(378, 23);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged_1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(25, 228);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(378, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(25, 316);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(378, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Comic Sans MS", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(84, 489);
+            label4.Name = "label4";
+            label4.Size = new Size(253, 38);
+            label4.TabIndex = 6;
+            label4.Text = "Witaj użytkowniku";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Teal;
+            label5.Font = new Font("Comic Sans MS", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label5.Location = new Point(33, 18);
+            label5.Name = "label5";
+            label5.Size = new Size(350, 45);
+            label5.TabIndex = 7;
+            label5.Text = "Rejestruj nowe konto";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(95, 397);
+            button1.Name = "button1";
+            button1.Size = new Size(226, 79);
+            button1.TabIndex = 8;
+            button1.Text = "REJESTRUJ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(432, 635);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private ColorDialog colorDialog1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+    }
+}
